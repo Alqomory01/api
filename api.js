14a13881407b2f5dcbf5c15f33c64bbeb3ce588f
Alqomory01 <Alqomorytech@gmail.com>
2025-04-24 15:28:10 +0100

    latest commit

--- api.js
+++ api.js
@@ -9,5 +9,28 @@ const getCountryData = function(country){
     }).then(function(data){
         console.log(data);
 
+        const countryData = data[0]
+        const apiRequest = `
+        <article class="country">
+        <img class="country__img" src="${countryData.flags.png}">
+        <div class="country__data">
+        <h3 class="country__name">${countryData.name.common}</h3>
+        <h4 class="country__region">${countryData.region}</h4>
+        <p class="country__row"><span>👫</span>${countryData.population}</p>
+
+        </div>
+        </article>`
+        countriesContainer.insertAdjacentHTML("beforeend", apiRequest)
+        countriesContainer.style.opacity= 1;
+
+
     });
 }
+getCountryData("spain")
+getCountryData("nigeria")
+// getCountryData("usa")
+
+//  function countryData(data){
+//     return data *2
+//  }
+// console.log(countryData(5));
